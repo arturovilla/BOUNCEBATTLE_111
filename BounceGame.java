aca8d25ab0a1f0e3af602254138e4ae68ee74cf8
arturovilla <rturovilla@tamu.edu>
2021-03-09 16:04:51 -0600

    Update BounceGame.java
    just a note

--- BounceGame.java
+++ BounceGame.java
@@ -12,7 +12,12 @@ class BounceBattle
 {
     public static void main(String[] args)
     {
-
-    }
-}
+        //lets hold off on the actual coding part 
+        // i want to gwt our rules down then kinda spread the work
+        // if you already cloned the repo you can delete that from your machine 
+        // it would probably be helpful if we all met on some day soon and talked about 
+        // how working together is actually going to work and then we can get everyone set up and 
+        // on the same. 
+    }//end main
+}//end class
 
