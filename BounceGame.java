620fe7e30a849ac7c8fe045877dbfc716fd067a9
arturovilla <rturovilla@tamu.edu>
2021-03-15 15:28:40 -0500

    choice block made
    print staments where instuctions and choices are laid out

--- BounceGame.java
+++ BounceGame.java
@@ -17,7 +17,24 @@ class BounceBattle
         // if you already cloned the repo you can delete that from your machine 
         // it would probably be helpful if we all met on some day soon and talked about 
         // how working together is actually going to work and then we can get everyone set up and 
-        // on the same page. 
+        // on the same page.
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nHello! welcome to bounce battle!\nThe game mode you pick will decide what the rules will be.");
+        System.out.println("\n(1) BIRD'S EYE TIC-TAC-TOE BATTLE!\n\t2 players -- Be the first to get three balls in-a-row as seen from above the cube;\n\tonly the top balls in any space count (block your opponent!).");
+        System.out.println("\n(2) LUCKY 13 BATTLE!\n\t2 players -- Be the first to bounce 13 balls into any location in the cube.");
+        System.out.println("\n(3) GREAT WALL BATTLE\n\t2 players -- Be the first to bounce nine balls into the wall closest to you.\n\tIn this battle, it does not matter which ball colors are used.\n\tIf you bounce in your opponent’s wall, however, the ball stays in place.");
+        System.out.println("\n(4) POINT BALL BATTLE\n\t2 players -- Be the first to score 21 points.  Points are scored by bouncing balls into the cube.\n\tThe front vertical wall is worth one point per ball, the middle vertical wall is worth two points per ball and \n\tthe back vertical wall is worth three points per ball.\n\tEach player gets to bounce three balls per turn.  Each player gets the same amount of turns.\n\tAfter each round, add up the score and play additional rounds as necessary until there is a winner.");
+        System.out.println("\nPlease choose from one of the game modes above. (enter 1-4)");
+        int choice  = input.nextInt();
+        System.out.println(choice);
+
+
+
+
+
+
     }//end main
 }//end class
 
